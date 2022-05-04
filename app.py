66560ad345faff60692f2a8e6fa3a07ac96d4708
danylo <danikfox1616@gmail.com>
2022-05-04 20:37:31 +0300

    adding new methods for creating teachers name, found problem using logs
    Co-authored-by: Myroslav Toloshnyi <Miroslavetsh@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,25 +1,48 @@
 import redis
 from flask import Flask, render_template, request
 import redis_sql
+import logging
 
+logging.basicConfig(filename="logfile.txt",
+                    filemode='w',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+
+logging.debug("Logging test...")
 
 
 app = Flask(__name__)
 
-@app.route('/', methods=['get','post'])
+@app.route('/')
 def index():
-    if request.method == "POST":
-        pass
-    else:
-        return render_template('index.html')
+    logging.warning('route index')
+    return render_template('index.html')
 
 @app.route('/teachers', methods=['get','post'])
 def teachers():
-    if request.method == "post":
-        output = request.form.get('full_name')
+    logging.warning('route teachers post1 method')
+    if request.method == "post": # не працює!!!!!!!
+        output = request.form['full_name']
+        logging.warning('route teachers post2 method')
+        logging.error(output)
         return render_template('teachers.html', lessons=redis_sql.get_lessons(output))
     else:
+        logging.warning('route teachers get method')
         return render_template('teachers.html')
+
+@app.route("/create_teacher", methods=['get','post'])
+def create():
+    if request.method == "post":
+        output = request.form.get('new_name')
+        logging.warning('route create teacher post method')
+        logging.error(output)
+        redis_sql.add_teacher(output)
+        return render_template('index.html')
+    else:
+        logging.warning('route create teacher get method')
+        return render_template('create_teacher.html')
+
 
 @app.route('/changing_sc', methods=['get','post'])
 def changing_sc():
--- redis_sql.py
+++ redis_sql.py
@@ -1,6 +1,6 @@
 import redis
 from redis.commands.json.path import Path
-
+import uuid
 
 def get_teacher_id(full_name):
     with redis.Redis() as client:
@@ -12,19 +12,37 @@ def get_lesson(full_name):
     lessons = []
     subjects = []
     with redis.Redis() as client:
+        
         for i in client.json().get('subjects'):
             if i['teacherId'] == get_teacher_id(full_name):
                 subjects.append(i["id"])
         for i in client.json().get('pairs'):
             if i["subjectId"] in subjects:
                 lessons.append(i)
+    print(lessons)
     return lessons
 
 def changing_sc(changes_list):
     with redis.Redis() as client:
         for i in changes_list:
             if changes_list[i] != None:
-                client.json().set(i ,Path.root_path(), changes_list[i])
+                client.json().set(i, Path.root_path(), changes_list[i])
+
+def check_not_existing_teacher(name):
+    with redis.Redis() as client:
+        try:
+            teachers = client.json().get("teachers")
+            for i in teachers:
+                if i["name"] == name:
+                    return False
+        except:
+            return True
+
+def add_teacher(name):
+    if check_not_existing_teacher(name):
+        jsonfile = {'id':uuid.uuid4(), "name":name}
+        with redis.Redis() as client:
+            client.json().set("teachers", Path.root_path(), jsonfile)
 
 
 
