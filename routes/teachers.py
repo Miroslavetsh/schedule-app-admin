e50344fdb2dbdef34beb754addb0f7c2f012eef5
Miroslavetsh <dfgshte@gmail.com>
2022-05-05 19:55:23 +0300

    Update teachers template and C operation

--- routes/teachers.py
+++ routes/teachers.py
@@ -1,7 +1,7 @@
 
 from flask import Blueprint, render_template, request
 
-from redis_workers.teachers import get_teachers_list, add_teacher
+from redis_workers.teachers import get_teacher_by_name, get_teachers_list, add_teacher
 
 teachers_page = Blueprint('/teachers', __name__, template_folder='templates')
 
@@ -9,6 +9,7 @@ teachers_page = Blueprint('/teachers', __name__, template_folder='templates')
 @teachers_page.route('/', methods=['get'])
 def get_teachers_page():
     teachers = get_teachers_list()
+
     return render_template('teachers.html', teachers=teachers)
 
 
@@ -16,9 +17,10 @@ def get_teachers_page():
 def add_teacher_page():
 
     if request.method == "POST":
-        output = request.form.to_dict()
-        add_teacher(output['teacher_name'])
+        form_parameters = request.form.to_dict()
+        add_teacher(form_parameters['teacher_name'])
+        teachers = get_teachers_list()
 
-        return render_template('index.html')
+        return render_template('teachers.html', teachers=teachers)
     else:
-        return render_template('create_teacher.html')
+        return render_template('teachers.html', teachers=teachers)
