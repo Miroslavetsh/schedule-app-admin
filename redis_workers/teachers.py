c9c3e1b88f32effcb49735dd23c346d2138e372e
Miroslavetsh <dfgshte@gmail.com>
2022-05-05 10:17:52 +0300

    Refactor teachers worker

---
+++ redis_workers/teachers.py
@@ -0,0 +1,36 @@
+import uuid
+import redis
+from redis.commands.json.path import Path
+
+
+def check_if_do_not_exist_in_db(name):
+    return get_teacher_by_name(name) == None
+
+
+def get_teacher_by_name(name):
+    with redis.Redis() as client:
+        for teacher in client.json().get('teachers'):
+            if teacher['name'] == name:
+                return teacher
+            return None
+
+
+def add_teacher(name):
+    if check_if_do_not_exist_in_db(name):
+        teacher_data = {'id': str(uuid.uuid4()), "name": name}
+
+        with redis.Redis() as client:
+            client.json().arrappend('teachers', Path.root_path(), teacher_data)
+            return True
+
+    else:
+        return False
+
+
+def get_all_teachers():
+    with redis.Redis() as client:
+        client.json().arrappend('teachers', Path.root_path())
+
+
+# TODO: update teacher
+# TODO: delete teacher
