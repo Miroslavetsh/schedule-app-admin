cea5b78442c94a5648702483b1ee3db7c7264c05
Miroslavetsh <dfgshte@gmail.com>
2022-05-24 22:25:19 +0300

    Prepare teacher's routes endpoint

--- routes/teachers.py
+++ routes/teachers.py
@@ -1,4 +1,5 @@
 
+import json
 from flask import Blueprint, render_template, request
 from redis_workers import base
 from redis_workers.teachers import get_teachers_list, add_teacher
@@ -7,34 +8,22 @@ teachers_page = Blueprint('/teachers', __name__, template_folder='templates')
 
 
 @teachers_page.route('/', methods=['get'])
-def get_teachers_page():
+def get_all_teachers():
     teachers = get_teachers_list()
 
-    return render_template('teachers.html', teachers=teachers)
+    return json.dumps(teachers)
 
 
-@teachers_page.route("/add", methods=['get', 'post'])
-def add_teacher_page():
-
-    if request.method == "POST":
-        form_parameters = request.form.to_dict()
-        add_teacher(form_parameters['teacher_name'])
-        teachers = get_teachers_list()
-
-        return render_template('teachers.html', teachers=teachers)
-    else:
-        return render_template('teachers.html', teachers=teachers)
+@teachers_page.route("/create", methods=['post'])
+def create_teacher():
+    pass
 
 
-@teachers_page.route('/<teacher_id>/update', methods=['delete', 'patch','post'])
-def update_teacher_page(teacher_id):
-    form_parameters = request.form.to_dict()
-    teachers = base.update("teachers", id=teacher_id,
-     name=form_parameters['name'])
-    return render_template('teachers.html', teachers=teachers)
+@teachers_page.route('/<teacher_id>/update', methods=['delete', 'patch', 'post'])
+def update_teacher(teacher_id):
+    pass
 
 
-@teachers_page.route('/<teacher_id>/delete', methods=['delete', 'post'])
-def delete_teacher_page(teacher_id):
-    teachers = base.delete("teachers", id=teacher_id)
-    return render_template('teachers.html', teachers=teachers)
+@teachers_page.route('/<teacher_id>/delete', methods=['delete', 'patch', 'post'])
+def delete_teacher(teacher_id):
+    pass
