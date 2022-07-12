f0d60c0b33beb83ad1e03b976bcc1161ebf6b82a
Miroslavetsh <dfgshte@gmail.com>
2022-07-12 12:41:22 +0300

    Sync id field with incoming one

--- models/teacher.py
+++ models/teacher.py
@@ -1,9 +1,6 @@
-import uuid
-
-
 class Teacher(object):
     tablename = "teachers"
 
-    def __init__(self, name):
-        self.id = str(uuid.uuid4())
+    def __init__(self, id, name):
+        self.id = id
         self.name = name
